35932481617be2080ffe30ebe933077c89ef1e30
adriik <adriik5@gmail.com>
2018-02-01 19:21:00 +0100

    Dodaj pliki projektów.

---
+++ Ignis_Web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Ignis_Web.Models;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ignis_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var lista = new List<Czlonek>();
+            NpgsqlConnection cn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["IgnisTabs"].ConnectionString);
+            cn.Open();
+            string QueryPeople = "SELECT public.\"People\".\"Nickname\" FROM public.\"People\"";
+            using (NpgsqlCommand command = new NpgsqlCommand(QueryPeople, cn))
+            {
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        lista.Add(new Czlonek(reader.GetString(0)));
+                    }
+                }
+            }
+            ViewBag.People = lista;
+            cn.Close();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Ignis_Web/Models/Czlonek.cs
@@ -0,0 +1,52 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Ignis_Web.Models
+{
+    public class Czlonek
+    {
+        public string Nickname { get; set; }
+        public string Class { get; set; }
+
+        public string Pierwszy { get; set; }
+        public string Drugi { get; set; }
+        public string Trzeci { get; set; }
+        public string Czwarty { get; set; }
+        public string Piaty { get; set; }
+        public string Kolor { get; set; }
+
+        public Czlonek(string Nickname)
+        {
+            this.Nickname = Nickname;//[0].ToString().ToUpper() + Nickname.Substring(1);
+            if(Nickname == "Etze")
+            {
+                this.Kolor = "background-color: coral;";
+            }
+            
+            //s = s[0].ToUpper() + s.Substring(1);
+            NpgsqlConnection cn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["IgnisTabs"].ConnectionString);
+            cn.Open();
+            string QueryPeople = "SELECT \"First Boss\",\"Second Boss\",\"Third Boss\",\"Fourth Boss\",\"Fifth Boss\" FROM public.\"IBP\" WHERE \"Nickname\"=" + "'"+Nickname+"'";
+            using (NpgsqlCommand command = new NpgsqlCommand(QueryPeople, cn))
+            {
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Pierwszy = reader.GetString(0);
+                        Drugi = reader.GetString(1);
+                        Trzeci = reader.GetString(2);
+                        Czwarty = reader.GetString(3);
+                        Piaty = reader.GetString(4);
+                    }
+                }
+            }
+            cn.Close();
+            
+        }
+    }
+}
