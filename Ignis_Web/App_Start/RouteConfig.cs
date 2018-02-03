095d7e12af35de88ad88431e5aaa0c9edefaa8c7
adriik <adriik5@gmail.com>
2018-02-03 16:14:07 +0100

    Dodanie strony logowania

--- Ignis_Web/App_Start/RouteConfig.cs
+++ Ignis_Web/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace Ignis_Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+            name: "Login",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+            );
         }
     }
 }
