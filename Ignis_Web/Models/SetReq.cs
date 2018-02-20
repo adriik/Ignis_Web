e5e47d5d2310a6dcd8a4a6e0e36a957235f6e007
Bartlomiej Lewinski <33038366+BLewinski13@users.noreply.github.com>
2018-02-20 18:41:41 +0000

    Dodanie itemow healowych oraz rozpoczecie pracy przy zmiennych dotyczacych zbierania setow

---
+++ Ignis_Web/Models/SetReq.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ignis_Web.Models
+{
+    public class SetReq
+    {
+        public string Nick { get; set; }
+        public string Set { get; set; }
+        public string username { get; set; }
+        public bool collect { get; set; }
+        public string Nickname { get; set; }
+
+        public SetReq(string Nickname)
+        {
+            this.Nickname = Nickname;//[0].ToString().ToUpper() + Nickname.Substring(1);
+        }
+
+    }
+}
